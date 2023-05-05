629a618b213326e9af81571a8c3c49ab057edc21
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-05-04 22:28:36 -0300

    Implementação da camada de apresentação

---
+++ Umfg.WebApi/Controllers/ProdutoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umfg.Dominio.Interfaces;
+
+namespace Umfg.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v2/produto")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoServico _servico;
+
+        [HttpGet]
+        public IActionResult ObterTodos() => Ok(_servico.ObterTodos());
+    }
+}

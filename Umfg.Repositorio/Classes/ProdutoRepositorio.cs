5620f7091c4e19df961121ec0d5dc80bd1044dd5
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-05-04 21:26:36 -0300

    Implementações em aula

---
+++ Umfg.Repositorio/Classes/ProdutoRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umfg.Dominio.Entidades;
+using Umfg.Dominio.Interfaces;
+
+namespace Umfg.Repositorio.Classes
+{
+    public sealed class ProdutoRepositorio : IProdutoRepositorio
+    {
+        private readonly List<Produto> _lista = new List<Produto>()
+        {
+            new Produto()
+            {
+                CodigoBarra = "1001",
+                Descricao = "Coca-Cola KS",
+                Valor = 3.49m,
+            },
+            new Produto()
+            {
+                CodigoBarra = "1002",
+                Descricao = "Coca-Cola Lata",
+                Valor = 3.99m,
+            },
+        };
+
+        public void Adicionar(Produto produto)
+        {
+            _lista.Add(produto);
+        }
+
+        public List<Produto> ObterTodos()
+        {
+            return _lista;
+        }
+
+        public void Remover(Produto produto)
+        {
+            _lista.Remove(produto);
+        }
+    }
+}
---
+++ Umfg.Servicos/Servicos/ProdutoServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umfg.Dominio.Entidades;
+using Umfg.Dominio.Interfaces;
+
+namespace Umfg.Aplicacao.Servicos
+{
+    public class ProdutoServico : IProdutoServico
+    {
+        public Produto Gravar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterProdutoPorCodigoBarra(string codigoBarra)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverPorCodigoBarra(string codigoBarra)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
